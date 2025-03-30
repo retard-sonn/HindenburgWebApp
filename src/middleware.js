06bbad6fe2e074dbe380257f569e5cfc2ed5e53e
Abraar <86068038+retard-sonn@users.noreply.github.com>
2025-03-30 08:08:30 +0530

    Add files via upload

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "bd0241d4-488a-4bc2-b87f-b1f39cc94c2e");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
