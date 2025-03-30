4f8aabf5be07674fa159cbe3ddfaea79cd7ec54c
Abraar <86068038+retard-sonn@users.noreply.github.com>
2025-03-30 08:44:06 +0530

    Update page.jsx

--- src/app/hindenburg-uncensored-ai/page.jsx
+++ src/app/hindenburg-uncensored-ai/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 
-import { useHandleStreamResponse } from "../../utilities/runtime-helpers";
+import { useHandleStreamResponse } from "../utilities/runtime-helpers";
 
 function MainComponent() {
   const [messages, setMessages] = useState([
